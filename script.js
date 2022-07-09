dfe808669021c9732846d1c1a1ba78de3c73d2c4
MaiAlshammari <mayoosh_1987@hotmail.com>
2022-07-09 18:55:50 +0300

    done

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
 // عرف المصفوفة هنا
 
-function addCourse() {
-  let name = document.getElementById("name").value;
-  let id = document.getElementById("id").value;
-  let phone = document.getElementById("phone").value;
-  let course = document.getElementById("course").value;
-  
-}
+const courses =[];
 
-function load() {
-  let container = document.getElementById("container");
+function addCourse() {
+    let name = document.getElementById("name").value;
+    let id = document.getElementById("id").value;
+    let phone = document.getElementById("phone").value;
+    let course = document.getElementById("course").value;
+    let newCourse=[];
+    newCourse.push({name,id,phone,course});
+    newCourse=courses;
+    load();
+    save();
+  }
   
-}
+  function load() {
+    let container = document.getElementById("container");
+courses.forEach((courses) =>
+{
+    container.innerHTML +=`<div>
+<h1>${courses.name} - ${courses.id}</h1>
+    <h3>${item.course}</h3>
+    </div>`
+});
+ }
+// بونص
+ courses.pop();
+
+ function save()
+ {
+
+    localStorage.setItem("courses",JSON.stringify(courses));
+
+ }
+  
+ function read()
+ {
+    courses= JSON.parse(localStorage.getItem("courses")|| []);
+    load();
+ }
+
+ read();
+
+//  بونص
+ localStorage.removeItem("courses");
